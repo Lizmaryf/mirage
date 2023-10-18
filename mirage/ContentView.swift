ab566754484d2ad6a72bfae8b047e7da88c61e67
lizmaryf <lizmaryfernandez7@gmail.com>
2023-10-18 11:39:23 -0400

    Initial Commit

---
+++ mirage/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mirage
+//
+//  Created by Lizmary Fernandez on 10/18/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mirage/mirageApp.swift
@@ -0,0 +1,17 @@
+//
+//  mirageApp.swift
+//  mirage
+//
+//  Created by Lizmary Fernandez on 10/18/23.
+//
+
+import SwiftUI
+
+@main
+struct mirageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
