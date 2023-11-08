d0f57ad8cb6bcc4d6a707ff45b3134b8ebbd2f8a
Dewayne Williams <jujubean3157@gmail.com>
2023-11-08 08:28:48 -0500

    adding chapter

--- mirage/mirageApp.swift
+++ mirage/mirageApp.swift
@@ -15,3 +15,82 @@ struct mirageApp: App {
         }
     }
 }
+let petient2 = [
+    "name" : "Oliva Jackson",
+    "birthyear" : 1905 ,
+    "birthmonth" : "October",
+    "birthday" : 23 ,
+    "HairColor" : "white",
+    "EyeColor" : "Gray",
+    "Husband" : "Jason Cotton",
+    "Doctor": "Brown"
+    
+    ]
+
+struct Hotel {
+    var name : String
+    var street : String
+    var number : Int
+    var zip : Int
+    var city : String
+var State: String
+}
+var marageHotel = Hotel(name: "Marage", street: "Gotham", number: 1366, zip: 13456, city: "Chicago", State: "Indiana")
+
+func HowIGotHere() {
+    print ("""
+           "Excuseme doctor but I wish to have my wife comited.
+            Im aoeey bur aisw deom dwq inrewarinf pweaonliry rerwa rhwew ia no ewaon ro xommir hwe.
+            I amd deeply concerned about my wife and is willing to give you anything you need to help her.
+            I'm glad that we are agrred and commited in helping hiis you lady.. I hope you have a plan to get her to my hospital.
+            That's simple. She's not going to a hospital, sje
+           s gpomg tp a jpte;. bky dthe time she figures out where she is you should be able to hold her for one reason odr anouther.
+           """)
+            }
+            print ("""
+            Honey, would you like to go on a trip with me?
+            """)
+            
+            func LittleTrip ()
+            let yes = true
+            let no = false
+            if yes {
+        print ("Sure,I would love to go on a vacation")}
+            else no {
+        print ("Sorry,I am too busy to go on vacation")}
+
+    }
+            
+     
+        func wherearewegoing()
+        print ("""
+Where are going on vacation to? I am takeing to the \("name") hotel at\("number") \("street"). Is that not locattec in \("city") \("state")?
+Yep.
+""")
+
+            
+            func ArrivalAndGreeting(){
+        print ("hello welcometo the marage the Marage Hotel.")
+        print ("May I take your bages and give you a tour")
+             
+            let rooms = ["Garden" , "Dinning Room" ."Kitchen" . "Recrection roon", "Meeting Rooms" , "Your Room"]
+                        "
+            var Comp = ["Nice" , "Impressve" , "Impressive", "pritty" ,"I approve"]
+            
+            for rtour in tour { }
+            for rcomp in resp {}
+            
+            print ("""
+this is our \("rtour") what do you think? I think it is \("rcomp").
+""")
+        
+        
+            
+            
+}
+
+
+
+
+    
+    
